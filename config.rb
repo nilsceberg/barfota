ef72b6270de4654b294335259a8a9343c1c54386
Nils Ceberg <nils.ceberg@gmail.com>
2015-08-09 22:45:38 +0200

    Set up project

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+sass_dir = 'sass'
+css_dir = 'app/assets/stylesheets'
+images_dir = 'app/assets/images'
+javascripts_dir = 'app'
+relative_assets = true
+line_comments = true
